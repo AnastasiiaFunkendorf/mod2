b0c3e7ab34ed66b8ac60451c5386368752f78127
AnastasiiaFunkendorf <45520691+AnastasiiaFunkendorf@users.noreply.github.com>
2019-03-03 15:00:14 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+	
+	
+	
+	$('.slider-container').slick({
+	  slidesToShow: 3,
+	  slidesToScroll: 1,
+	  autoplay: true,
+	  autoplaySpeed: 2000,
+	  pauseOnHover: true,
+	  dots: true,
+	});
+
+	
+
+});
+
